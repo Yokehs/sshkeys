f61362bc4f753225e0b45a5bbb5e0562964f19c8
Yokeh <starkovkonstantin@mail.ru>
2024-05-30 22:47:46 +0400

    some shit

--- script.sh
+++ script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-echo 'Running Script'
+echo "Running Script"
 DIR=~/.ssh/
+BACKUP_DIR
 KEYS=*.pub
+
+#---
+echo "Backup $DIR in $BACKUP_DIR"
+cp $DIR $BACKUP_DIR
+echo "Done"
+#---
 echo "Check $DIR"
 if ! [ -d $DIR ]; then
 echo "Make dir $DIR"
@@ -9,8 +16,13 @@ mkdir $DIR
 else
 echo "Dir= $DIR is assigned"
 fi
+#---
 echo "Copy keys"
 cp $KEYS $DIR
 echo "Done"
-
+#---
+echo "Add in authorized_keys"
+cat *.pub >> $DIR/authorized_keys
+cat $DIR/authorized_keys
+echo "Done"
 
